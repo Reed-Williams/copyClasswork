d41a8342264e431d1f20919909333d299c6f285a
Reed-Williams <williamc18@newpaltz.edu>
2022-02-19 12:11:15 -0500

    npm setup

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,24 @@
-const http = require('http');
+const express = require('express')
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express()
+const port = 3000
 
-//createServer is a method from the http lib, we pass is a reqest and response
-//fat arrow means we are passing a function as a parameter
-//no return value bc these are all async functions
-//this is old syntax
-//calling end is the real return(end of the return value) bc you are calling functions on the response object
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-//listen is registering with the OS
-//this is code that gets run in the case of a success
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app
+    .get('/', (req, res) => {
+        res.send('You are on the homepage');
+    })
+    .get('/about', (req, res) => {
+        res.send('You are on the about page');
+    })
+    .get('/contact', (req, res) => {
+        res.send({
+            email: 'williamc18@newpaltz.edu',
+            phone: '123-456-7890',
+            twitter: '@casasafrass',
+            instagram: '@casasafrass'
+        });
+    })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
