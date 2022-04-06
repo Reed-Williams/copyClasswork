bb8152e3877ed63d41f45864b0759db80e63f207
Reed-Williams <williamc18@newpaltz.edu>
2022-04-06 10:41:41 -0400

    posts and auth

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -1,3 +1,43 @@
-//now we will have post information here
-//( I will copy and past later)
-//main structure was just copied and pasted from users.js, with different content inside
+const express = require('express');
+const app = express.Router();
+
+const postModel = require('../models/posts');
+
+const CREATED_STATUS = 201;
+
+app
+    .get('/', (req, res) => {
+        res.send(postModel.list);
+    })
+    .get('/wall', (req, res) => {
+
+        res.send(postModel.list.filter(post => post.owner === req.user.id ));
+    })
+    .get('/:id', (req, res) => {
+
+        const post = postModel.get(req.params.id);
+        res.send(post);
+
+    })
+    .post('/', (req, res) => {
+        const post = postModel.create(req.body);
+        res.status(CREATED_STATUS).send(post);
+    })
+    .delete('/:id', (req, res) => {
+
+        const post = postModel.remove(req.params.id);
+
+        res.send({ success: true, errors: [], data: post });
+
+    })
+    .patch('/:id', (req, res) => {
+
+        const post = postModel.update(req.params.id, req.body );
+
+        res.send({ success: true, errors: [], data: post });
+
+    })
+
+
+
+module.exports = app;
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+
+const userModel = require('./models/users');
 const usersController = require('./controllers/users');
+const postsController = require('./controllers/posts');
+const { requireAuth } = require( './models/auth') ;
+
 const app = express()
 const port = process.env.PORT || 3000
 app
@@ -8,12 +13,36 @@ app
     .use('/', express.static(__dirname + '/public/'))
     //lets the server parse incomming json text
     .use(express.json())
+    .use((req, res, next) => {
+      const auth = req.headers.authorization;
+      if(auth){
+        //splitting string and taking sexond element
+        const token = auth.split(' ')[1];
+        //fromToken returns a promise, se we need to use the then and the next
+        userModel.fromToken(token)
+        .then(user => {
+          req.user = user;
+          next();
+        }).catch(next)
+      }
+      else{
+        next();
+      }
+    })
+
     //we will have /api/ in the url when we are using the api
     .get('/api/', (req, res) => {
         res.send('You are at the root of the api for the best class ever - ' + process.env.BEST_CLASS_EVER);
     })
     //call the users controller file to return user info
     .use('/api/users', usersController)
+    .use('/api/posts', requireAuth, postsController)
+    .use((err, req, res, next)=>{
+      console.error(err);
+
+      res.status(err.statusCode || 500 )
+         .send({ errors: [err.message ?? 'Internal server error']})
+    })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
